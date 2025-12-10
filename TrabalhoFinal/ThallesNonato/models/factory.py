d2cb2ba92bbe3030d24d82a860c5cc4bfb37c109
ThallesNonato1123 <eusouthalles@gmail.com>
2025-12-09 22:38:04 -0300

    adicionado em modulos

---
+++ TrabalhoFinal/ThallesNonato/models/factory.py
@@ -0,0 +1,13 @@
+from models.dmf import DMF
+from models.ae import AutoRec
+
+def create_model(model_type, num_users, num_movies, device):
+    model_type = model_type.lower()
+
+    if model_type == "dmf":
+        return DMF(num_users=num_users, num_movies=num_movies).to(device)
+
+    if model_type == "ae":
+        return AutoRec(num_users=num_users, num_movies=num_movies).to(device)
+
+    raise ValueError(f"Modelo desconhecido: {model_type}")
