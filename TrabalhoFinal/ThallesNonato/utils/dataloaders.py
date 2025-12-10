7754bfe532c8c26aecfe20c18a32c503080d0477
ThallesNonato1123 <eusouthalles@gmail.com>
2025-12-09 23:48:57 -0300

    atualizado dataloaders

--- TrabalhoFinal/ThallesNonato/utils/dataloaders.py
+++ TrabalhoFinal/ThallesNonato/utils/dataloaders.py
@@ -1,7 +1,9 @@
 from torch.utils.data import DataLoader
 from datasets.NetflixDataset import NetflixDataset
+from datasets.UserDataset import UserDataset
 from sklearn.model_selection import train_test_split
-
+import numpy as np
+import torch
 
 def create_dataloaders(
     train_users, train_movies, train_ratings,
@@ -19,3 +21,13 @@ def create_dataloaders(
     test_loader  = DataLoader(test_dataset,  batch_size=batch_size, shuffle=False)
 
     return train_dataset, test_dataset, train_loader, test_loader
+
+def create_autorec_dataloader(train_matrix, batch_size=64):
+    R_filled = np.nan_to_num(train_matrix, nan=0.0)
+    R_tensor = torch.tensor(R_filled, dtype=torch.float32)
+
+    dataset = UserDataset(R_tensor)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    return dataloader
+
